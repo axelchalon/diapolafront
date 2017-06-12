0eefc8641d8a8080d0fc7e7480187f3be052e302
Axel Chalon <axel.chalon@hetic.net>
2017-06-12 16:39:10 +0200

    api ok

--- js/api.js
+++ js/api.js
@@ -1,6 +1,30 @@
 console.log('API OK')
 var API = {
-    getPresentationInfo: (id) => {
-        // return promise...
-    }
-};
+        _fetch: function (url, options) {
+            var headers = new Headers();
+            /* headers.append("X-JWT", "JWT_HERE"); */
+
+            return fetch(url, Object.assign({}, options, {
+                    headers: headers
+                })).
+                then((response) => {
+                    if (!response.ok) {
+                        throw Error(response.statusText);
+                    }
+                    return response.json();
+                })
+        },
+        getPresentationInfo: function (id) {
+            // return promise...
+        },
+        signUp: function (lastname, firstname, email, password, password_confirm) {
+            return this._fetch('http://echo.jsontest.com/key/value/one/two')
+                // simulate delay
+                .then((response) => {
+                    return new Promise(function(resolve,reject) {
+                        setTimeout(() => { resolve(response); }, 2000)       
+                    })
+                
+                })
+            }
+        };
