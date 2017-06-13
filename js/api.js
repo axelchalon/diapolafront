65b3d51131e6be7a4c62ff68a6897bc6497def84
Axel Chalon <axel.chalon@hetic.net>
2017-06-13 16:07:35 +0200

    ruuuu

--- js/api.js
+++ js/api.js
@@ -1,8 +1,21 @@
 console.log('API OK')
+const IS_LOCAL = false;
+const HOST = IS_LOCAL ? 'http://lpython.dev' : 'http://ec2-13-58-190-26.us-east-2.compute.amazonaws.com'
+
+var qs = params => Object.keys(params)
+    .map(k => encodeURIComponent(k) + '=' + encodeURIComponent(params[k]))
+    .join('&');
+
 var API = {
+    jwtToken: undefined,
     _fetch: function (url, options) {
-        var headers = new Headers();
-        /* headers.append("X-JWT", "JWT_HERE"); */
+        if (options.headers)
+            var headers = options.headers;
+        else
+            var headers = new Headers();
+        
+        if (typeof this.jwtToken !== 'undefined')
+            headers.append("X-JWT", this.jwtToken);
 
         return fetch(url, Object.assign({}, options, {
             headers: headers
@@ -30,6 +43,10 @@ var API = {
             })
     },
     logIn: function (email, password) {
-        return this._fetch('http://echo.jsontest.com/key/value/one/two')
+        return this._fetch(HOST + '/api-token-auth/', {
+            method: 'POST',
+            headers: {'Content-Type':'application/x-www-form-urlencoded'},
+            body: qs({username: email, password})
+        }).then((res) => { this.jwtToken = res.token; return res; })
     }
 }
