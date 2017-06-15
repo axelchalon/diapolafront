4c4db50a3f51ff7a3235172a592cd5447827892a
Axel Chalon <axel.chalon@hetic.net>
2017-06-15 17:26:48 +0200

    css

--- js/api.js
+++ js/api.js
@@ -13,7 +13,7 @@ var API = {
             var headers = options.headers;
         else
             var headers = {};
-        
+
         if (typeof this.jwtToken !== 'undefined')
             headers["Authorization"] = "JWT " + this.jwtToken;
 
@@ -27,62 +27,146 @@ var API = {
             return response.json();
         })
     },
-    logOut: function() {
+    logOut: function () {
         this.jwtToken = undefined;
         return Promise.resolve();
     },
-    addCollaborator: function(email) {
-        return this._fetch(HOST + '/users/invite', {method: 'POST',
-                            headers: {'Content-Type':'application/json'}, body: JSON.stringify({email: email})
-                                                        
-                                                        });
+    addCollaborator: function (email) {
+        return this._fetch(HOST + '/users/invite', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                email: email
+            })
+
+        });
     },
     getPresentationInfo: function (id) {
-        return this._fetch(HOST + '/presentations/'+id);  
+        return this._fetch(HOST + '/presentations/' + id);
     },
     deletePresentation: function (id) {
-        return this._fetch(HOST + '/presentations/'+id, {method: 'DELETE'});  
+        return this._fetch(HOST + '/presentations/' + id, {
+            method: 'DELETE'
+        });
     },
-    renamePresentation: function (newname,id) {
-        return this._fetch(HOST + '/presentations/'+id, {method: 'PUT',
-                            headers: {'Content-Type':'application/json'}, body: JSON.stringify({title: newname})
-                                                        
-                                                        });  
+    renamePresentation: function (newname, id) {
+        return this._fetch(HOST + '/presentations/' + id, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                title: newname
+            })
+
+        });
     },
-    search: function(q) {
+    search: function (q) {
         return this._fetch(HOST + '/slides/?q=' + encodeURIComponent(q));
     },
-    searchP: function(q) {
+    searchP: function (q) {
         return this._fetch(HOST + '/presentations/?q=' + encodeURIComponent(q));
     },
-    getPresentations: function() {
+    getPresentations: function () {
         return this._fetch(HOST + '/presentations/');
     },
-    addTagToSlide: function(tag,id) {
-        return this._fetch(HOST + '/slides/' + id + '/tags', { method: 'POST', 
-            headers: {'Content-Type':'application/json'}, body: JSON.stringify({action: 'add', tag: tag})});
+    addTagToSlide: function (tag, id) {
+        return this._fetch(HOST + '/slides/' + id + '/tags', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                action: 'add',
+                tag: tag
+            })
+        });
     },
-    addTagToPresentation: function(tag,id) {
-        return this._fetch(HOST + '/presentations/' + id + '/tags', { method: 'POST', 
-            headers: {'Content-Type':'application/json'}, body: JSON.stringify({action: 'add', tag: tag})});
+    importPresentation: function (title, comment, tags, file) {
+        var data = new FormData()
+        data.append('file', file);
+        data.append('title', title);
+        data.append('comment', comment);
+
+        return this._fetch(HOST + '/presentations/', {
+            method: 'POST',
+            
+            body: data
+        }).then(function(res) {
+            console.log('composition new ok',res)
+            
+            var tagPromises = tags.map((tag) =>                 this.addTagToPresentation(tag,res.id)
+                );
+            
+            return Promise.all(tagPromises);
+        });
+
     },
-    removeTagFromSlide: function(tag,id) {
-        return this._fetch(HOST + '/slides/' + id + '/tags', { method: 'POST', 
-            headers: {'Content-Type':'application/json'}, body: JSON.stringify({action: 'delete', tag: tag})});
+    addTagToPresentation: function (tag, id) {
+        return this._fetch(HOST + '/presentations/' + id + '/tags', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                action: 'add',
+                tag: tag
+            })
+        });
     },
-    removeTagFromPresentation: function(tag,id) {
-        return this._fetch(HOST + '/presentations/' + id + '/tags', { method: 'POST', 
-            headers: {'Content-Type':'application/json'}, body: JSON.stringify({action: 'delete', tag: tag})});
+    removeTagFromSlide: function (tag, id) {
+        return this._fetch(HOST + '/slides/' + id + '/tags', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                action: 'delete',
+                tag: tag
+            })
+        });
+    },
+    removeTagFromPresentation: function (tag, id) {
+        return this._fetch(HOST + '/presentations/' + id + '/tags', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                action: 'delete',
+                tag: tag
+            })
+        });
     },
     signUp: function (lastname, firstname, email, password, password_confirm) {
-         return this._fetch(HOST + '/users/', { method: 'POST', 
-            headers: {'Content-Type':'application/json'}, body: JSON.stringify({first_name: firstname, last_name: lastname, username: email, password})});
+        return this._fetch(HOST + '/users/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                first_name: firstname,
+                last_name: lastname,
+                username: email,
+                password
+            })
+        });
     },
     logIn: function (email, password) {
         return this._fetch(HOST + '/api-token-auth/', {
             method: 'POST',
-            headers: {'Content-Type':'application/x-www-form-urlencoded'},
-            body: qs({username: email, password})
-        }).then((res) => { this.jwtToken = res.token; return res; })
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: qs({
+                username: email,
+                password
+            })
+        }).then((res) => {
+            this.jwtToken = res.token;
+            return res;
+        })
     }
 }
